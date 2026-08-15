115e84171a8539d2f0f58708e521263c7e185bea
auhong <auhong@cdnexis.com>
2026-08-15 19:34:22 +0800

    Update MonitorDashboard.js

--- src/views/MonitorDashboard.js
+++ src/views/MonitorDashboard.js
@@ -325,15 +325,15 @@ function MonitorDashboard() {
                       ))}
                     </Input>
                   </FormGroup>
+                  {selectedDeviceData && selectedDeviceData.sensors.GPS && (
+                    <p className="mb-0 text-dark">
+                      {t('monitorDashboard.gpsLocation')}: {selectedDeviceData.sensors.GPS}
+                    </p>
+                  )}
                 </Col>
                 {selectedDeviceData && (
                   <Col md="4" className="d-flex flex-column justify-content-center">
                     <p className="mb-0 text-muted">{t('monitorDashboard.lastSeen')}: {new Date(selectedDeviceData.lastTime).toLocaleString()}</p>
-                    {selectedDeviceData.sensors.GPS && (
-                      <p className="mb-0 text-dark">
-                        {t('monitorDashboard.gpsLocation')}: {selectedDeviceData.sensors.GPS}
-                      </p>
-                    )}
                   </Col>
                 )}
                 {selectedDeviceData && selectedDeviceData.sensors.GPS && (
